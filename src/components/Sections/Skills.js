53ae5d9e691575d5a5cc743f266c9227be417fa6
carpecodeum <adityabhatnagar14@gmail.com>
2020-11-11 19:00:17 +0530

    Add skills component

---
+++ src/components/Sections/Skills.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import skills from '../../configs/skills.json';
+
+import {
+  skills_parent,
+  skills_header,
+  skills_box,
+  skills_container,
+  icons_parent,
+  tooltiptext,
+  skills_images,
+  skill_heading,
+} from '../../styles/Skills.css';
+
+export default class Skills extends React.Component {
+  render() {
+    return (
+      <div className={skills_parent}>
+        <div className={skills_header}>My Skills</div>
+        <div className={skills_container}>
+          {Object.keys(skills).map(key => (
+            <>
+              <div className={skill_heading}>{key}</div>
+              <div className={skills_box}>
+                {skills[key].map(skill => (
+                  <div className={icons_parent}>
+                    <div className={tooltiptext}>{skill.name}</div>
+                    <img src={skill.icon} className={skills_images} />
+                  </div>
+                ))}
+              </div>
+            </>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
