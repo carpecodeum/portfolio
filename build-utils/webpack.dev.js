4a8e048fb88d1e99fed3a215ce7d0515f947a4ce
carpecodeum <adityabhatnagar14@gmail.com>
2020-11-12 01:21:51 +0530

    Add url-loader and change images directory

--- build-utils/webpack.dev.js
+++ build-utils/webpack.dev.js
@@ -1,48 +1,57 @@
-const commonPaths = require("./common-paths");
+const commonPaths = require('./common-paths');
 
-const webpack = require("webpack");
+const webpack = require('webpack');
 
 const port = process.env.PORT || 3000;
 
 const config = {
-  mode: "development",
+  mode: 'development',
   entry: {
     app: `${commonPaths.appEntry}/index.js`,
   },
   output: {
-    filename: "[name].[hash].js",
+    filename: '[name].[hash].js',
   },
   resolve: {
     alias: {
-      "react-dom": "@hot-loader/react-dom",
+      'react-dom': '@hot-loader/react-dom',
     },
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
         test: /\.css$/,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader',
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               modules: true,
-              localsConvention: "camelCase",
+              localsConvention: 'camelCase',
               sourceMap: true,
             },
           },
         ],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        include: commonPaths.appEntry,
+        exclude: /node_modules/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
     ],
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
-    host: "localhost",
+    host: 'localhost',
     port: port,
     historyApiFallback: true,
+    contentBase: commonPaths.appEntry,
     hot: true,
     open: true,
   },
